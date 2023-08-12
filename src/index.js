f7d60753885b1e7aac84a6d9f050dcc3694eab85
zahra zamanoghli <zahra.zamanoghli@gmail.com>
2023-08-12 22:11:14 +0330

    updated for hw7

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ const header = document.querySelector("h1");
 const input = document.querySelector("#city-input");
 const apiKey = "2980ff43226d67e53abfcdb6d457dcc8";
 const degree = document.querySelector("#degree");
+const toggle = document.querySelector("#toggle");
+const desc = document.querySelector("#desc");
+const humidity = document.querySelector("#humidity");
+const speed = document.querySelector("#speed");
+const icon = document.querySelector("#icon");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   header.innerHTML = input.value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then((res) => {
-    degree.innerHTML = res.data.main.temp;
+    degree.innerHTML = Math.round(res.data.main.temp);
+    desc.innerHTML = res.data.weather[0].description;
+    humidity.innerHTML = res.data.main.humidity;
+    speed.innerHTML = Math.round(res.data.wind.speed * 3.6);
+    icon.innerHTML = `<img
+          src="http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png"
+          alt=""
+          width="42"
+        />`;
   });
+  toggle.innerHTML = "C";
 });
 
 //2
@@ -44,3 +58,17 @@ function getCurrLoc() {
 function cb(e) {
   console.log(e);
 }
+
+function toggleFunc() {
+  let unit = document.getElementById("toggle");
+
+  if (unit.innerHTML == "C") {
+    unit.innerHTML = "F";
+    degree.innerHTML = Math.round((parseInt(degree.innerHTML) * 9) / 5 + 32);
+  } else {
+    unit.innerHTML = "C";
+    degree.innerHTML = Math.round(((parseInt(degree.innerHTML) - 32) * 5) / 9);
+  }
+}
+
+toggle.addEventListener("click", toggleFunc);
