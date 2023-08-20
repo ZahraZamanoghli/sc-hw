ac8856e936cb62232996c2e75d21bd54beb02bc1
zahra zamanoghli <zahra.zamanoghli@gmail.com>
2023-08-20 23:09:50 +0330

    final project

--- src/index.js
+++ src/index.js
@@ -2,13 +2,36 @@
 const form = document.querySelector("#city-search");
 const header = document.querySelector("h1");
 const input = document.querySelector("#city-input");
-const apiKey = "2980ff43226d67e53abfcdb6d457dcc8";
+const apiKey = "8161b4309ee03faae957729ba7104797";
 const degree = document.querySelector("#degree");
 const toggle = document.querySelector("#toggle");
 const desc = document.querySelector("#desc");
 const humidity = document.querySelector("#humidity");
 const speed = document.querySelector("#speed");
 const icon = document.querySelector("#icon");
+
+function getForecast(coord) {
+  let forecast = document.querySelector("#forecast");
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=metric`;
+  axios.get(`${apiUrl}`).then((res) => {
+    console.log(res);
+    let s = ``;
+    res.data.daily.forEach((x, i) => {
+      if (i < 5) {
+        s += `<div class="col">
+      <span>${days[(now.getDay() + i) % 7]}</span>
+      <img
+          src="http://openweathermap.org/img/wn/${x.weather[0].icon}@2x.png"
+          alt=""
+          width="42"
+        /> 
+        <span>${Math.round(x.temp.min)}° | ${Math.round(x.temp.max)}°</span>
+        </div>`;
+      }
+    });
+    forecast.innerHTML = s;
+  });
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -24,6 +47,7 @@ form.addEventListener("submit", (e) => {
           alt=""
           width="42"
         />`;
+    getForecast(res.data.coord);
   });
   toggle.innerHTML = "C";
 });
